d430bda9d936f42e4c7fbb6bc598d7c066997d45
MorganReyS <morgan.rey@etu.u-bordeaux.fr>
2025-03-20 22:58:41 +0100

    Update TER_ski.cpp
    Constructeur modifié avec la bonne syntaxe de Graphe + ajout fonction pour init le graphe pour algo FF

--- src/TER_ski.cpp
+++ src/TER_ski.cpp
@@ -1,4 +1,4 @@
-#include "test.hpp"
+#include "TER_ski.hpp"
 
 
 TER_ski::TER_ski(string filename)
@@ -17,7 +17,7 @@ TER_ski::TER_ski(string filename)
             if (temp == "NODES")
             {
                 inf >> numNodes;
-                graph.resize(numNodes);
+                Graphe.resize(numNodes);
             }
             else if (temp == "LINKS")
             {
@@ -25,7 +25,7 @@ TER_ski::TER_ski(string filename)
                 double capacity = 1.0;
                 while (inf >> node1 >> node2)
                 {
-                    if (graph[node1].find(node2) != graph[node1].end())
+                    if (Graphe[node1].find(node2) != Graphe[node1].end())
                     {
                         doublons.emplace_back(node1, node2);
                     }
@@ -35,11 +35,32 @@ TER_ski::TER_ski(string filename)
                         arc.capacity = capacity;
                         arc.flow = 0.0;
                         arc.residual = capacity;
-                        graph[node1][node2] = arc;
+                        Graphe[node1][node2] = arc;
                     }
                 }
             }
         }
         size = numNodes;
         inf.close();
+    }
+
+
+
+void TER_ski::restreindre_graphe(const vector<vector<double>> &Xij)        // Modifie GFord pour avoir son graphe restreint aux xij = 0; avec les sommets dédoublés
+    {
+        int size = Graphe.size();
+        GFord.resize(0);
+        GFord.resize(2*size);
+        double capacity = 1.;
+        
+        for (int i=0; i<size; i++){
+            Arc a = {capacity, 0., capacity};
+            GFord[i][i+size] = a;
+            for (auto &[s, arc] : Graphe[i]){
+                if (Xij[i][s] < 0.5){
+                    GFord[size + i][s] = arc;
+                }
+            }
+        }
+        symmetrize(GFord);
     }
