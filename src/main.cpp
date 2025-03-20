731ea3c5bb47d9cf6a3955e0bcfefb330c6e332b
MorganReyS <morgan.rey@etu.u-bordeaux.fr>
2025-03-20 23:02:55 +0100

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include "TER_ski.hpp"
+
+int main(){
+
+
+    string filename = "test.txt";
+    TER_ski graphe(filename);
+
+    vector<int> som_tri = graphe.triTopo();
+
+
+
+
+
+
+    vector<vector<double>> xij;
+    graphe.restreindre_graphe(xij);
+
+    int s = 0;
+    bool fin = false;
+    while (s<graphe.size-1 && !fin){
+        int t = s+1;
+        while (t<graphe.size && !fin){
+            double flow = fordfulkerson(graphe.GFord, s, t);
+            if (flow > 1.5){
+                fin = true;
+                // ajout de contraintes
+            } else {
+                reset_graph(graphe.GFord);
+            }
+        }
+    }
+
+
+
+
+
+
+    return 0;
+}
