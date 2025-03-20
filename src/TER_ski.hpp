36723f372de003b3194bd0bead970e7b446ab633
MorganReyS <morgan.rey@etu.u-bordeaux.fr>
2025-03-20 19:15:09 +0100

    Update TER_ski.hpp

--- src/TER_ski.hpp
+++ src/TER_ski.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <unordered_map>
 #include <iostream>
+#include <utility>
 
 using namespace std;
 
@@ -20,46 +21,14 @@ public:
     vector<unordered_map<int, Arc>> graph;
     vector<pair<int, int>> doublons;
 
-    TER_ski(string filename)
-    {
-        ifstream inf(filename);
-        if (!inf)
-        {
-            size = -1;
-            return;
-        }
+    TER_ski(string filename);
 
-        string temp;
-        int numNodes = -1;
-        while (inf >> temp)
-        {
-            if (temp == "NODES")
-            {
-                inf >> numNodes;
-                graph.resize(numNodes);
-            }
-            else if (temp == "LINKS")
-            {
-                int node1, node2;
-                double capacity = 1.0;
-                while (inf >> node1 >> node2)
-                {
-                    if (graph[node1].find(node2) != graph[node1].end())
-                    {
-                        doublons.emplace_back(node1, node2);
-                    }
-                    else
-                    {
-                        Arc arc;
-                        arc.capacity = capacity;
-                        arc.flow = 0.0;
-                        arc.residual = capacity;
-                        graph[node1][node2] = arc;
-                    }
-                }
-            }
-        }
-        size = numNodes;
-        inf.close();
-    }
+    vector<int> triTopo();
+
+    double FordFulkerson();
+
+    double Resolution();
+
+
+    
 };
