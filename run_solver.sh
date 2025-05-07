4574b055e58cef452a2a0435f946d8ca5bebcd1a
Simon Fraile <92757823+fraile1129@users.noreply.github.com>
2025-05-07 16:01:12 +0200

    Update run_solver.sh

--- run_solver.sh
+++ run_solver.sh
@@ -3,33 +3,37 @@
 data_dir="data"
 log_dir="logs"
 solver="./Release/gurobiSolverCWLP"
+fres="test.txt"
 
 if [ ! -f "$solver" ]; then
   echo "Erreur : l'exécutable $solver est introuvable"
   exit 1
 fi
 
-mkdir -p "$log_dir"
+if [ ! -f "$fres" ]; then
+  echo "Nom;Version;BP;BD;Gap;Temps" > $fres
+fi
+
 
 # Boucle sur les combinaisons
-for X in C R; do
-  for Y in {1..8}; do
-    for Z in {1..10}; do
-      for W in {1..9}; do
-        for v in 0 2; do 
+for Y in 5 1 6 2 7 3 8 4; do # taille, nb de sommets # 5-60 1/6-120 2/7-250 3/8-50 4-1000
+  for W in {1..9}; do # nb de arcs (densité)
+    for Z in {1..3}; do # instance diff
+      for v in -1 0 2 11; do 
+        for X in C R; do
           filename="bologne-${X}1-BPPC_${Y}_0_${Z}.txt_0.${W}"
           filepath="${data_dir}/${filename}"
-          logfile="${log_dir}/${filename}_model${v}.log"
+          #logfile="${log_dir}/${filename}_model${v}.log"
 
           # Ne pas relancer si log existe déjà
-          if [ -f "$logfile" ]; then
-            echo "Déjà traité : $filename avec modèle $v"
-            continue
-          fi
+          # if [ -f "$logfile" ]; then
+          #   echo "Déjà traité : $filename avec modèle $v"
+          #   continue
+          # fi
 
           if [ -f "$filepath" ]; then
             echo "→ Lancement : $filename avec modèle $v"
-            "$solver" "$filename" "$v" > "$logfile" 2>&1
+            "$solver" "$filename" "$v" #> "$logfile" 2>&1
           else
             echo "Fichier introuvable : $filepath"
           fi
